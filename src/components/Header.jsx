b0e8e875bd8d7ce454f270b06884b149152786cc
Ataby <matabay200069@gmail.com>
2024-09-25 03:10:28 +0300

    animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import Person from "../assets/icons/person.svg"
 
 const Header = () => {
   return (
-    <div className='flex justify-between mx-8'>
+    <div className='flex justify-between p-8  bg-[#f3f2f6]'>
       <div className='flex  items-center '>
 <img src={Plane} alt="" className='border rotate-90 bg-purple-700 p-1 rounded-full' />
       <p className='text-2xl text-gray-700 font-semibold'>PLANE SCAPE</p>
@@ -16,9 +16,9 @@ const Header = () => {
       <div className='flex gap-4 '>
  
       
-      <button className='flex text-gray-700 items-center'><img src={TagFilled} alt="" />Deals</button>
-      <button className='flex text-gray-700 items-center'> <img src={World} alt="" />Discover</button>
-      <button className='flex text-gray-700 items-center'> <img src={Person} alt="" />Joane Smith</button>
+      <button className='flex text-gray-700 items-center transition-transform duration-300 hover:scale-110'><img src={TagFilled} alt="" />Deals</button>
+      <button className='flex text-gray-700 items-center transition-transform duration-300 hover:scale-110'> <img src={World} alt="" />Discover</button>
+      <button className='flex text-gray-700 items-center transition-transform duration-300 hover:scale-110'> <img src={Person} alt="" />Joane Smith</button>
  
       </div>
     </div>
